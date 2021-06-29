9601b5b00c87ac841a86c337500ec429a6cc4a1c
Carlinhao <carsilva.ti@gmail.com>
2021-06-28 22:09:01 -0300

    Creating method to send email

---
+++ SalesWebMvc/src/SalesMvc.Web/Libraries/Email/ContactEmail.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Mail;
+using SalesMvc.Web.Models;
+
+namespace SalesMvc.Web.Libraries.Email
+{
+    public class ContactEmail
+    {
+        public static void SendContactEmail(Contact contact)
+        {
+            /*
+             * Server tha send email 
+             */
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential("","");
+            smtpClient.EnableSsl = true;
+
+            MailMessage mailMessage = new MailMessage();
+        }
+    }
+}
