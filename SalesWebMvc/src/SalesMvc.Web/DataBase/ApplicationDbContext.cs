ac3516d5982355bd89d34489887222fd644763f6
Carlinhao <carsilva.ti@gmail.com>
2021-07-05 21:21:45 -0300

    add class to access database

--- SalesWebMvc/src/SalesMvc.Web/DataBase/ApplicationDbContext.cs
+++ SalesWebMvc/src/SalesMvc.Web/DataBase/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SalesMvc.Web.Models;
 
 namespace SalesMvc.Web.DataBase
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Costumer> Costumers { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
         {
