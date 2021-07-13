8c9c0061d9cfb421303a21493eaa38d263c5cdae
Carlinhao <carsilva.ti@gmail.com>
2021-07-12 22:33:35 -0300

    Refactor property name to DbSet

--- SalesWebMvc/src/SalesMvc.Web/DataBase/ApplicationDbContext.cs
+++ SalesWebMvc/src/SalesMvc.Web/DataBase/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace SalesMvc.Web.DataBase
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Costumer> Costumers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
