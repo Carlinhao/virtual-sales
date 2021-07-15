4c1ed2e25528ecec0cbc136ba001e4f6d0d46366
Carlinhao <carsilva.ti@gmail.com>
2021-07-14 23:02:39 -0300

    Add data anotations

--- SalesWebMvc/src/SalesMvc.Web/Models/NewsLetterEmail.cs
+++ SalesWebMvc/src/SalesMvc.Web/Models/NewsLetterEmail.cs
@@ -1,8 +1,14 @@
-﻿namespace SalesMvc.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+using SalesMvc.Web.Libraries.Lang;
+
+namespace SalesMvc.Web.Models
 {
     public class NewsLetterEmail
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Message), ErrorMessage = "MSG_E001")]
+        [EmailAddress(ErrorMessageResourceType = typeof(Message), ErrorMessage = "MSG_E004")]
         public string Email { get; set; }
     }
 }
