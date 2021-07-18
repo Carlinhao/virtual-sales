c3047cac1a74367dff9a51140327e9c8073d5133
Carlinhao <carsilva.ti@gmail.com>
2021-07-18 20:55:55 -0300

    Create concrete class NewsLetterEmailRepository

---
+++ SalesWebMvc/src/SalesMvc.Web/Repositories/NewsLetterEmailRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesMvc.Web.DataBase;
+using SalesMvc.Web.Models;
+using SalesMvc.Web.Repositories.Interfaces;
+
+namespace SalesMvc.Web.Repositories
+{
+    public class NewsLetterEmailRepository : INewsLetterEmailRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<NewsLetterEmail> _dBset;
+        public NewsLetterEmailRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dBset = _context.Set<NewsLetterEmail>();
+        }
+
+        public async Task<IEnumerable<NewsLetterEmail>> GetAllAsync()
+        {
+            return await _dBset.ToListAsync();
+        }
+
+        public async Task RegisterEmail(NewsLetterEmail newsLetter)
+        {
+            await _dBset.AddAsync(newsLetter);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
