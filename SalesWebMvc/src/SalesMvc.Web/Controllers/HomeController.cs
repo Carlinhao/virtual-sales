80cde33fb587baa65883d051c52d319571e144a6
Carlinhao <carsilva.ti@gmail.com>
2021-07-28 23:43:50 -0300

    Teste create session.

--- SalesWebMvc/src/SalesMvc.Web/Controllers/HomeController.cs
+++ SalesWebMvc/src/SalesMvc.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SalesMvc.Web.Libraries.Email;
 using SalesMvc.Web.Models;
@@ -31,7 +32,7 @@ namespace SalesMvc.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Index([FromForm] NewsLetterEmail newsLetterEmail)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 await _newsLetterEmailRepository.RegisterEmail(newsLetterEmail);
 
@@ -58,7 +59,7 @@ namespace SalesMvc.Web.Controllers
                     Name = HttpContext.Request.Form["name"],
                     Email = HttpContext.Request.Form["email"],
                     Text = HttpContext.Request.Form["text"]
-                };               
+                };
 
                 var errorList = new List<ValidationResult>();
                 var context = new ValidationContext(contact);
@@ -85,7 +86,7 @@ namespace SalesMvc.Web.Controllers
             catch (Exception)
             {
                 ViewData["MSG_E"] = "Ops.. something is wrong!";
-                
+
                 // TODO - Write log
             }
 
@@ -98,11 +99,30 @@ namespace SalesMvc.Web.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Painel()
+        {
+            byte[] userID;
+
+            if (HttpContext.Session.TryGetValue("ID", out userID))
+            {
+                return new ContentResult() { Content = "Welcome to painel!" + userID[0] };
+            }
+            else
+            {
+                return new ContentResult() { Content = "Access Denied" };
+            }
+        }
+
         [HttpPost]
         public IActionResult Login([FromForm] Customer customer)
         {
-            if(customer.Email == "teste@teste.com" && customer.Password == "1234")
+            if (customer.Email == "teste@teste.com" && customer.Password == "1234")
             {
+                HttpContext.Session.Set("ID", new byte[] { 52 });
+                HttpContext.Session.SetString("Email", customer.Email);
+                HttpContext.Session.SetInt32("Password", 1234);
+
                 return new ContentResult() { Content = "Welcome!" };
             }
             else
@@ -123,7 +143,7 @@ namespace SalesMvc.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-               await _repository.CreateAsync(customer);
+                await _repository.CreateAsync(customer);
 
                 TempData["MSG_S"] = "Register with success!";
 
