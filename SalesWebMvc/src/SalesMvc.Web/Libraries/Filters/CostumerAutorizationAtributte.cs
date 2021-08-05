ec393179f1669654383c971147456c21353adadd
Carlinhao <carsilva.ti@gmail.com>
2021-08-04 23:01:41 -0300

    Creating class CostumerAuthorization

---
+++ SalesWebMvc/src/SalesMvc.Web/Libraries/Filters/CostumerAutorizationAtributte.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SalesMvc.Web.Libraries.Filters
+{
+    /*
+     * Tipos de Filtro:
+     * - Authorization
+     * - Resource
+     * - Action
+     * - Execution
+     * - Result
+     */
+
+    public class CostumerAutorizationAtributte : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
