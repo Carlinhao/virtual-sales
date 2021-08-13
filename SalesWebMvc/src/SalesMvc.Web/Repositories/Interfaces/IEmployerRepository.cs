3f6fd9193dfb8acaeecf579ff2dfbd0c698658ca
Carlinhao <carsilva.ti@gmail.com>
2021-08-12 23:05:19 -0300

    Create interface IEmployerRepository.

---
+++ SalesWebMvc/src/SalesMvc.Web/Repositories/Interfaces/IEmployerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SalesMvc.Web.Models;
+
+namespace SalesMvc.Web.Repositories.Interfaces
+{
+    public interface IEmployerRepository
+    {
+        Task CreateAsync(Employer employer);
+        Task UpdateAsync(Employer employer);
+        Task DeleteAsync(int id);
+
+        Task<Employer> GetEmployerByIdAsync(int id);
+        Task<IEnumerable<Employer>> GetAllEmployer();
+    }
+}
