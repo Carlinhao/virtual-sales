c3db68f5932d7b81ac18b2abd7fb328772957521
Carlinhao <carsilva.ti@gmail.com>
2021-08-24 23:01:35 -0300

    Create homeController to employee area

---
+++ SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesMvc.Web.Areas.Employee.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
