d4b879655eb984311f3699e39e0d926a2fe816dd
Carlinhao <carsilva.ti@gmail.com>
2021-08-25 21:58:00 -0300

    Create and implementing class to manager logn employee

---
+++ SalesWebMvc/src/SalesMvc.Web/Libraries/Login/LoginEmployee.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using SalesMvc.Web.Libraries.Sessions;
+using SalesMvc.Web.Models;
+
+namespace SalesMvc.Web.Libraries.Login
+{
+    public class LoginEmployee
+    {
+        private readonly string _key = "Login.Employee";
+        private readonly Session _sesseion;
+
+        public LoginEmployee(Session sesseion)
+        {
+            _sesseion = sesseion;
+        }
+
+        public void PostCostumer(Customer costumer)
+        {
+            var value = JsonSerializer.Serialize(costumer);
+
+            _sesseion.Create(_key, value);
+        }
+
+        public Customer GetCustomer()
+        {
+            if (_sesseion.Exist(_key))
+            {
+                return JsonSerializer.Deserialize<Customer>(_sesseion.Search(_key));
+            }
+            return null;
+        }
+
+        public void Logout()
+        {
+            _sesseion.RemoveAll();
+        }
+    }
+}
