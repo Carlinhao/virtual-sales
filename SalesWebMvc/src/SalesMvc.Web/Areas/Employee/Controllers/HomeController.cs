357e3cfd4c2f9b953ce64b120b61153f734c31c1
Carlinhao <carsilva.ti@gmail.com>
2021-08-26 23:29:32 -0300

    Implemeting method to login Employee.

--- SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/HomeController.cs
+++ SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/HomeController.cs
@@ -1,20 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesMvc.Web.Libraries.Login;
+using SalesMvc.Web.Repositories.Interfaces;
 
 namespace SalesMvc.Web.Areas.Employee.Controllers
 {
     [Area("Employee")]
     public class HomeController : Controller
     {
+        private readonly IEmployeeRepository _repository;
+        private readonly LoginEmployee _loginEmployee;
+
+        public HomeController(IEmployeeRepository repository, LoginEmployee loginEmployee)
+        {
+            _repository = repository;
+            _loginEmployee = loginEmployee;
+        }
+
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        public IActionResult Painel()
+        {
+            return new ContentResult() { Content = "Welcome to painel!" };
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromForm] Models.Employee employee)
+        {
+            var employeeRepo = await _repository.Login(employee.Email, employee.Password);
+
+            if (employeeRepo != null)
+            {
+                _loginEmployee.PostEmplyee(employeeRepo);
+
+                return new RedirectResult(Url.Action(nameof(Painel)));
+            }
+            else
+            {
+                ViewData["MSG_E"] = "User or Password invalid.";
+                return View();
+            }
+        }
+
+        [HttpPost]
         public IActionResult RecoverPassword()
         {
             return View();
         }
 
+        [HttpPost]
         public IActionResult CreateNewPassword()
         {
             return View();
