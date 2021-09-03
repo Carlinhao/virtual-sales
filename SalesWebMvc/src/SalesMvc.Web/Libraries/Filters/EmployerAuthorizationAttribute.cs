1f7bbf5b62c08034ace3763b1ff4ec403ba8060e
Carlinhao <carsilva.ti@gmail.com>
2021-09-02 23:19:25 -0300

    Create EmployerAuthorizationAttribute to validate user access.

---
+++ SalesWebMvc/src/SalesMvc.Web/Libraries/Filters/EmployerAuthorizationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SalesMvc.Web.Libraries.Login;
+
+namespace SalesMvc.Web.Libraries.Filters
+{
+    public class EmployerAuthorizationAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var loginEmployee = (LoginEmployee)context.HttpContext.RequestServices.GetService(typeof(LoginEmployee));
+            var result = loginEmployee.GetEmplyee();
+
+            if (result == null)
+            {
+                context.Result = new ContentResult() { Content = "Access Denied" };
+            }
+        }
+    }
+}
