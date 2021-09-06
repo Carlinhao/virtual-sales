f877991256c4e6961b133f49ee6276dc944142f8
Carlinhao <carsilva.ti@gmail.com>
2021-09-05 22:43:21 -0300

    Create Entity Category and your properties;

---
+++ SalesWebMvc/src/SalesMvc.Web/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesMvc.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug {  get; set; }
+        public int CategoryFatherId { get; set; }
+
+        [ForeignKey("CategoryFatherId")]
+        public virtual Category CategoryFather { get; set; }
+    }
+}
