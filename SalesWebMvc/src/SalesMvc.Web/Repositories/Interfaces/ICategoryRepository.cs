09c265517e8a358148ecf9cdad997bec04391e7e
Carlinhao <carsilva.ti@gmail.com>
2021-09-15 23:57:44 -0300

    Add intefaces methods

--- SalesWebMvc/src/SalesMvc.Web/Repositories/Interfaces/ICategoryRepository.cs
+++ SalesWebMvc/src/SalesMvc.Web/Repositories/Interfaces/ICategoryRepository.cs
@@ -1,10 +1,14 @@
 using System.Threading.Tasks;
 using SalesMvc.Web.Models;
+using System.Collections.Generic;
 
 namespace SalesMvc.Web.Repositories.Interfaces
 {
     public interface ICategoryRepository 
     {
         Task CreateAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<Category>> GetAllCategory();
     }
 }
