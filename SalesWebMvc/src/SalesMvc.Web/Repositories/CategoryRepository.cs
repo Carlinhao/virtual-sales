2076a84f632575a3e7ee4ca1ee31d26eba08193e
Carlinhao <carsilva.ti@gmail.com>
2021-09-17 22:24:19 -0300

    Creating concrete class

--- SalesWebMvc/src/SalesMvc.Web/Repositories/CategoryRepository.cs
+++ SalesWebMvc/src/SalesMvc.Web/Repositories/CategoryRepository.cs
@@ -1,7 +1,38 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SalesMvc.Web.DataBase;
+using SalesMvc.Web.Models;
+using SalesMvc.Web.Repositories.Interfaces;
+
 namespace SalesMvc.Web.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private readonly ApplicationDbContext _context;
 
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task CreateAsync(Category category)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllCategory()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(Category category)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
