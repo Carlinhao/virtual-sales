582ecf0891b53de2f984a0ccf798af108dfc2ecc
Carlinhao <carsilva.ti@gmail.com>
2021-09-21 23:54:58 -0300

    Patemeters in constructor

--- SalesWebMvc/src/SalesMvc.Web/Repositories/CategoryRepository.cs
+++ SalesWebMvc/src/SalesMvc.Web/Repositories/CategoryRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SalesMvc.Web.DataBase;
 using SalesMvc.Web.Models;
 using SalesMvc.Web.Repositories.Interfaces;
@@ -9,10 +10,12 @@ namespace SalesMvc.Web.Repositories
     public class CategoryRepository : ICategoryRepository
     {
         private readonly ApplicationDbContext _context;
+        private readonly DbSet<Category> _dbset;
 
         public CategoryRepository(ApplicationDbContext context)
         {
             _context = context;
+            _dbset = _context.Set<Category>();
         }
 
         public Task CreateAsync(Category category)
