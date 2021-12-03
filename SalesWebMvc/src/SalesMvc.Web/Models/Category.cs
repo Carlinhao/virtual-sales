47ab2e9150118d8947d4b652d2f0178ddeb75002
Carlinhao <carsilva.ti@gmail.com>
2021-12-02 23:47:34 -0300

    Add message validation

--- SalesWebMvc/src/SalesMvc.Web/Models/Category.cs
+++ SalesWebMvc/src/SalesMvc.Web/Models/Category.cs
@@ -1,11 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SalesMvc.Web.Libraries.Lang;
 
 namespace SalesMvc.Web.Models
 {
     public class Category
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+        [MinLength(4, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E002")]
+        // TODO - Create validation Name is unic
         public string Name { get; set; }
+
+        [Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+        [MinLength(4, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E002")]
         public string Slug {  get; set; }
         public int? CategoryFatherId { get; set; }
 
