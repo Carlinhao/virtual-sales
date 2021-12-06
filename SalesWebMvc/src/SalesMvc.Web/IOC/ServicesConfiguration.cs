4cc163cee334ac0b2ec0f3019ee667ec53bdb4eb
Carlinhao <carsilva.ti@gmail.com>
2021-12-05 23:27:33 -0300

    Add extension class IServiceCollection to StartUp

---
+++ SalesWebMvc/src/SalesMvc.Web/IOC/ServicesConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SalesMvc.Web.Libraries.Login;
+using SalesMvc.Web.Libraries.Sessions;
+using SalesMvc.Web.Repositories.Interfaces;
+using SalesMvc.Web.Repositories;
+
+namespace SalesMvc.Web.IOC
+{
+    public static class ServicesConfiguration
+    {
+        public static IServiceCollection GetServicesConfig(this IServiceCollection services)
+        {
+            // Repositories
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<INewsLetterEmailRepository, NewsLetterEmailRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            // Session
+            services.AddMemoryCache();
+            services.AddSession(opt =>
+            {
+            });
+            services.AddScoped<Session>();
+            services.AddScoped<LoginCostumer>();
+            services.AddScoped<LoginEmployee>();
+
+            return services;
+        }
+    }
+}
