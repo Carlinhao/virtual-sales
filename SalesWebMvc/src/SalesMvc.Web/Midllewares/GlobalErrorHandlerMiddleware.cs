001e698d25a6587dc244433d7cfc1f158ec4e402
Carlinhao <carsilva.ti@gmail.com>
2021-12-16 23:56:33 -0300

    Create class GlobalErrorHandlerMiddleware

---
+++ SalesWebMvc/src/SalesMvc.Web/Midllewares/GlobalErrorHandlerMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System;
+
+namespace SalesMvc.Web.Midllewares
+{
+    public class GlobalErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestDelegate> _logger;
+
+        public GlobalErrorHandlerMiddleware(RequestDelegate next,
+                                            ILogger<RequestDelegate> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokAsync(HttpContext context)
+        {
+            if (context == null)
+                return;
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+
+            }
+        }
+    }
+}
