567a06043a4cd39e4fcbbb699f08917fd8e1e838
Carlinhao <carsilva.ti@gmail.com>
2022-01-13 20:08:53 -0300

    Create skeleton of class EmployeeController.

---
+++ SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesMvc.Web.Areas.Employee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] Models.Employee employee)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromForm] Models.Employee employee, int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
