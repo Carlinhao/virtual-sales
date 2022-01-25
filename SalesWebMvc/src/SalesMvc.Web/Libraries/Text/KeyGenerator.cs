5ec083a9ce3fff39a7dd57eeaaa5fa225d3ad574
Carlinhao <carsilva.ti@gmail.com>
2022-01-24 23:11:22 -0300

    Password generator.

---
+++ SalesWebMvc/src/SalesMvc.Web/Libraries/Text/KeyGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SalesMvc.Web.Libraries.Text
+{
+    public class KeyGenerator
+    {
+        public static string GetUniqueKey(int size)
+        {
+            char[] chars = "abcdefghijklmnopqrstuvxzABCDEFGHIJKLMNOPQRSTUVXZYW0123456789".ToCharArray();
+            byte[] data = new byte[size];
+
+            //using RSACryptoServiceProvider cripto = new RSACryptoServiceProvider();
+            
+
+            StringBuilder result = new StringBuilder();
+
+            foreach(var item in data)
+            {
+                result.Append(chars[item % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+    }
+}
