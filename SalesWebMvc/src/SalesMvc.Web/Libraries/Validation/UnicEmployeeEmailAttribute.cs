96ecd48fad5dffac0f8e5c2a5748fb4951fd48c0
Carlinhao <carsilva.ti@gmail.com>
2022-02-17 23:48:27 -0300

    Create attribute to validate email.

---
+++ SalesWebMvc/src/SalesMvc.Web/Libraries/Validation/UnicEmployeeEmailAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using SalesMvc.Web.Models;
+using SalesMvc.Web.Repositories.Interfaces;
+
+namespace SalesMvc.Web.Libraries.Validation
+{
+    public class UnicEmployeeEmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {            
+            string Email = (value as string).Trim();
+            IEmployeeRepository _employeeRepository = validationContext.GetService(typeof(IEmployeeRepository)) as IEmployeeRepository;
+            var result = _employeeRepository.GetEmployerEmail(Email);
+
+            Employee empl = (Employee)validationContext.ObjectInstance;
+
+            if(result.Count() > 1)
+                return new ValidationResult("E-mail already registered.");
+
+            if(result.Count() == 1 && empl.Id != result.FirstOrDefault().Id)
+                return new ValidationResult("E-mail already registered.");
+
+            return ValidationResult.Success;
+        }
+    }
+}
