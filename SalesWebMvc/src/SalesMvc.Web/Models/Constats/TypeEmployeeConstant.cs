3c8b2b40f7bde64311ae487979df0298b7691079
Carlinhao <carsilva.ti@gmail.com>
2022-02-28 15:17:56 -0300

    create class with constant to validate type employee

---
+++ SalesWebMvc/src/SalesMvc.Web/Models/Constats/TypeEmployeeConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesMvc.Web.Models.Constats
+{
+    public class TypeEmployeeConstant
+    {
+        public const string Common = "C";
+        public const string Manager = "M";
+    }
+}
