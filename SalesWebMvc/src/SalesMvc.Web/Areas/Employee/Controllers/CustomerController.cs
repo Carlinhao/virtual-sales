b4ac588871558c6366de2042fb59d6cad08fb046
Carlinhao <carsilva.ti@gmail.com>
2022-03-03 23:48:29 -0300

    Creating new controller.

---
+++ SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesMvc.Web.Models;
+using SalesMvc.Web.Repositories.Interfaces;
+using X.PagedList;
+
+namespace SalesMvc.Web.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<IActionResult> Index(int? page)
+        {
+            IPagedList<Customer> customers = await _customerRepository.GetAllCustomer(page);
+
+            return View(customers);
+        }
+
+        public IActionResult Activate()
+        {
+            return View();
+        }
+    }
+}
