b2a4e65ca05e8cebdf4d518e7700324f3978968c
Carlinhao <carsilva.ti@gmail.com>
2022-03-17 03:18:40 -0300

    Add class product

---
+++ SalesWebMvc/src/SalesMvc.Web/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace SalesMvc.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Value { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
