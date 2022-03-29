91363bb900e3a37e5931de87d194b2fb5cfb72b9
Carlinhao <carsilva.ti@gmail.com>
2022-03-28 23:50:16 -0300

    Create class Image

---
+++ SalesWebMvc/src/SalesMvc.Web/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesMvc.Web.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+    }
+}
