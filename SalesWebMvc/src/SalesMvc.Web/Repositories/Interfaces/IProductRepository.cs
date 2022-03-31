a757388ac705bde510dde7a61d9eafea37b9bf25
Carlinhao <carsilva.ti@gmail.com>
2022-03-31 00:10:48 -0300

    Add methods.

--- SalesWebMvc/src/SalesMvc.Web/Repositories/Interfaces/IProductRepository.cs
+++ SalesWebMvc/src/SalesMvc.Web/Repositories/Interfaces/IProductRepository.cs
@@ -1,7 +1,16 @@
-﻿namespace SalesMvc.Web.Repositories.Interfaces
+﻿using System.Threading.Tasks;
+using SalesMvc.Web.Models;
+using X.PagedList;
+
+namespace SalesMvc.Web.Repositories.Interfaces
 {
     public interface IProductRepository
     {
+        Task CreateAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
 
+        Task<Product> GetEmployerByIdAsync(int id);
+        Task<IPagedList<Product>> GetAllProduct(int? page, string search);
     }
 }
