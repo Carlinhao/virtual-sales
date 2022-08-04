4a6ab90f1e68dc7d38dd0621419085c5289deb2a
Carlinhao <carsilva.ti@gmail.com>
2022-08-03 23:31:09 -0300

    Implementing concrete class.

---
+++ SalesWebMvc/src/SalesMvc.Web/Repositories/ImageRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using SalesMvc.Web.Models;
+using SalesMvc.Web.Repositories.Interfaces;
+
+namespace SalesMvc.Web.Repositories
+{
+    public class ImageRepository : IImageRepository
+    {
+        public Task CreateAsync(Image image)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteImageProductAsync(int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
