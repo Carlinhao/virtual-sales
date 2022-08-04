68c39f7d7185fcecd4123bae14598375a4245729
Carlinhao <carsilva.ti@gmail.com>
2022-08-03 23:30:30 -0300

    creating interface and contracts.

---
+++ SalesWebMvc/src/SalesMvc.Web/Repositories/Interfaces/IImageRepository.cs
@@ -0,0 +1,12 @@
+﻿using SalesMvc.Web.Models;
+using System.Threading.Tasks;
+
+namespace SalesMvc.Web.Repositories.Interfaces
+{
+    public interface IImageRepository
+    {
+        Task CreateAsync(Image image);
+        Task DeleteAsync(int id);
+        Task DeleteImageProductAsync(int productId);
+    }
+}
