3d26168977857ec550ce8ab2dba5b6e891ab0d66
Carlinhao <carsilva.ti@gmail.com>
2022-10-30 22:53:07 -0300

    feat: Creating class ProductController.cs

---
+++ SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesMvc.Web.Areas.Employee.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
