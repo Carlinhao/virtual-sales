402d0a8ac40f10d53fea0ecba8835ba40d2bf853
Carlinhao <carsilva.ti@gmail.com>
2023-02-12 20:07:55 -0300

    feat: Creating controller that show products

--- SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/ProductController.cs
+++ SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/ProductController.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesMvc.Web.Repositories.Interfaces;
 
 namespace SalesMvc.Web.Areas.Employee.Controllers
 {
+    [Area("Employee")]
     public class ProductController : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
         {
-            return View();
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int? page, string search)
+        {
+            var result = await _productRepository.GetAllProduct(page, search);
+            return View(result);
+        }
+
+        public IActionResult Update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult Delete()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
-
