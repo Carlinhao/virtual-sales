8b10cc6325a66a65fda27c329c9ad956f2e4f924
Carlinhao <carsilva.ti@gmail.com>
2023-02-23 00:06:03 -0300

    refactor: Add new attributes to validation properties.

--- SalesWebMvc/src/SalesMvc.Web/Models/Product.cs
+++ SalesWebMvc/src/SalesMvc.Web/Models/Product.cs
@@ -1,29 +1,49 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using SalesMvc.Web.Libraries.Lang;
 
 namespace SalesMvc.Web.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
 
+		[Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+		public string Description { get; set; }
+
+		[Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
         [Column(TypeName = "decimal(18,2)")]
 		[Display(Name = "Price")]
 		public decimal Value { get; set; }
 
-        [Display(Name = "Quantity")]
+		[Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+		[Range(0,10000, ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E006")]
+		[Display(Name = "Quantity")]
         public int ProductQuantity { get; set; }
 
-        // Delivery
-        public double Peso { get; set; }
-        public double Hight { get; set; }
-        public double Large { get; set; }
-        public double Length { get; set; }
+		// Delivery
+		[Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+		[Range(0.01, 30, ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E006")]
+		public double Peso { get; set; }
+
+		[Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+		[Range(11, 105, ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E006")]
+		public double Hight { get; set; }
+
+		[Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+		[Range(2, 105, ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E006")]
+		public double Large { get; set; }
+
+		[Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
+		[Range(16, 105, ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E006")]
+		public double Length { get; set; }
 
 		// Relationship Entity
+		[Required(ErrorMessage = null, ErrorMessageResourceType = typeof(Message), ErrorMessageResourceName = "MSG_E001")]
 		[Display(Name = "Category")]
 		public int CategoryId { get; set; }
 
