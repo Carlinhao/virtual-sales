2f10f51cac6c89430bfb46286fec06bd18a5fd81
Carlinhao <carsilva.ti@gmail.com>
2023-03-03 23:00:27 -0300

    feat: Create class util to import and delete image.

--- SalesWebMvc/src/SalesMvc.Web/Libraries/File/FileManagement.cs
+++ SalesWebMvc/src/SalesMvc.Web/Libraries/File/FileManagement.cs
@@ -1,17 +1,31 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
 
-namespace SalesMvc.Web.Libraries.File
+namespace SalesMvc.Web.Libraries
 {
 	public class FileManagement
 	{
-		public static void SaveProductImage(IFormFile file)
+		public static string SaveProductImage(IFormFile file)
 		{
+			var fileName = Path.GetFileName(file.FileName);
+			var completePaht = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads/temp", fileName);
 
+			using var stream = new FileStream(completePaht, FileMode.Create);
+			file.CopyTo(stream);
+
+			return Path.Combine("/uploads/temp", fileName);
 		}
 
-		public static void DeleteProductImage(IFormFile file)
+		public static bool DeleteProductImage(string filePath)
 		{
+			var completePaht = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", filePath.TrimStart('/'));
+			if (File.Exists(completePaht)) 
+			{
+				File.Delete(completePaht);
+				return true;
+			}
 
+			return false;
 		}
 	}
 }
