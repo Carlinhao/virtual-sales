fe9cfeb14a5e6b41db893089da0f97134a3f69df
Carlinhao <carsilva.ti@gmail.com>
2023-03-03 23:00:02 -0300

    feat: Create new Controller to upload file.

---
+++ SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/ImageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SalesMvc.Web.Libraries;
+
+namespace SalesMvc.Web.Areas.Employee.Controllers
+{
+	[Area("Employee")]
+	[Controller]
+	public class ImageController : Controller
+	{
+		public IActionResult Import(IFormFile image)
+		{
+			var pathFile = FileManagement.SaveProductImage(image);
+			if (pathFile.Length > 0)
+				return Ok(new { caminho = pathFile });
+
+			return new StatusCodeResult(500);
+		}
+
+		public async Task<IActionResult> Delete(string pathFile)
+		{
+			if (FileManagement.DeleteProductImage(pathFile))
+				return Ok();
+
+			return BadRequest();
+		}
+	}
+}
