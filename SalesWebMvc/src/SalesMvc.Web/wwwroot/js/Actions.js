6efc33a6215edf18b4365a587611857fde9ede60
Carlinhao <carsilva.ti@gmail.com>
2023-03-20 00:11:17 -0300

    feat: Add function to upload image.

--- SalesWebMvc/src/SalesMvc.Web/wwwroot/js/Actions.js
+++ SalesWebMvc/src/SalesMvc.Web/wwwroot/js/Actions.js
@@ -6,4 +6,33 @@
         }
     });
     $('.money').mask('000.000.000.000.000,00', { reverse: true });
+
+    AjaxImageUpload();
 });
+
+function AjaxImageUpload() {
+    $(".img-upload").click(function () {
+        $(this).parent().find(".input-file").click();
+    });
+
+    $(".input-file").on('change', function () {
+        var files = $('.input-file').prop("files");
+        var form = new FormData();
+
+        form.append("file", files[0]);
+
+        $.ajax({
+            type: "POST",
+            url: "/Employee/Image/Import",
+            data: form,
+            contentType: false,
+            processData: false,
+            error: function () {
+                alert("Error when import image!");
+            },
+            success: function (data) {
+                alert("Success");
+            }
+        });
+    });
+}
