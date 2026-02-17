43fec11c8b877ce547850d9ce835df31b43e318e
Carlinhao <carsilva.ti@gmail.com>
2026-02-17 18:09:54 -0300

    refact(ImageController.cs): Identy code.

--- SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/ImageController.cs
+++ SalesWebMvc/src/SalesMvc.Web/Areas/Employee/Controllers/ImageController.cs
@@ -3,28 +3,28 @@ using SalesMvc.Web.Libraries.File;
 
 namespace SalesMvc.Web.Areas.Employee.Controllers
 {
-	[Area("Employee")]
-	public class ImageController : Controller
-	{
-		[HttpPost]
-		public IActionResult Import(IFormFile file)
-		{
-			if (file is null)
-				return BadRequest();
+    [Area("Employee")]
+    public class ImageController : Controller
+    {
+        [HttpPost]
+        public IActionResult Import(IFormFile file)
+        {
+            if (file is null)
+                return BadRequest();
 
-			var pathFile = FileManagement.SaveProductImage(file);
-			if (pathFile.Length > 0)
-				return Ok(new { caminho = pathFile });
+            var pathFile = FileManagement.SaveProductImage(file);
+            if (pathFile.Length > 0)
+                return Ok(new { caminho = pathFile });
 
-			return new StatusCodeResult(500);
-		}
+            return new StatusCodeResult(500);
+        }
 
-		public IActionResult Delete(string pathFile)
-		{
-			if (FileManagement.DeleteProductImage(pathFile))
-				return Ok();
+        public IActionResult Delete(string pathFile)
+        {
+            if (FileManagement.DeleteProductImage(pathFile))
+                return Ok();
 
-			return BadRequest();
-		}
-	}
+            return BadRequest();
+        }
+    }
 }
